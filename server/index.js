54629f6305f8c00b4a23addb7b0c2ccbffb42dc3
michaelmerrill <michael@michaelmerrill.io>
2017-02-16 16:34:00 -0800

    Update index response

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080
 require('./middleware/middleware')(app)
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!')
+  res.send('Hello, Amazon Elastic Beanstalk!')
 })
 
 app.listen(port, () => {

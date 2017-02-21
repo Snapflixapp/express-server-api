1bd72db4bc24636bca4bb3bdb17043cc4ef20f5e
michaelmerrill <michael@michaelmerrill.io>
2017-02-21 15:20:04 -0800

    Testing docker

--- server/index.js
+++ server/index.js
@@ -3,22 +3,26 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const routes = require('./routes')
-const writeError = require('./helpers').writeError
+// const routes = require('./routes')
+// const writeError = require('./helpers').writeError
 
 const port = process.env.PORT || 8080
 
 require('./middleware/middleware')(app)
 
-app.use(routes)
-
-app.use(function (err, req, res, next) {
-  if (err && err.status) {
-    writeError(res, err)
-  } else {
-    next(err)
-  }
+app.get('/', function (req, res) {
+  res.send('Hello, World!')
 })
+
+// app.use(routes)
+
+// app.use(function (err, req, res, next) {
+//   if (err && err.status) {
+//     writeError(res, err)
+//   } else {
+//     next(err)
+//   }
+// })
 
 app.listen(port, () => {
   console.log('Listening on port ' + port)

7f28434968ce6944ac5152434ab9b31e31ef2c27
michaelmerrill <michael@michaelmerrill.io>
2017-02-20 22:36:14 -0800

    Refactor user auth

--- server/db/index.js
+++ server/db/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const neo4j = require('neo4j-driver').v1
 
 let driver = neo4j.driver(process.env.NEO4J_DEV, neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD))

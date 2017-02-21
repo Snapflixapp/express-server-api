acea29a593723229e5e5c7c67590123e47d6c249
michaelmerrill <michael@michaelmerrill.io>
2017-02-21 11:30:47 -0800

    Add test for databases

--- test/index.js
+++ test/index.js
@@ -1,12 +1,35 @@
-/* global describe, it */
+/* global describe, it, afterEach */
+'use strict'
 
-var app = require('../server')
-var chai = require('chai')
-var request = require('supertest')
+const app = require('../server')
+const chai = require('chai')
+const request = require('supertest')
+const session = require('../server/db').getSession(request)
 
-var expect = chai.expect
+const expect = chai.expect
 
-describe('Login', function () {
+describe('Register user', function () {
+  it('should create session for valid user', function (done) {
+    request(app)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .send({ username: 'username', password: 'password' })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .expect(function (response) {
+        expect(response.body).not.to.be.empty
+        expect(response.body).to.be.an('object')
+      })
+      .end(done)
+  })
+})
+
+describe('Log in user', function () {
+  afterEach(function () {
+    session.run('MATCH (user:User {username: {username}}) DELETE user', {username: 'username'})
+  })
+
   it('should create session for valid user', function (done) {
     request(app)
       .post('/login')

36cad1520abfb3d41ae760977015ae0d60020e55
michaelmerrill <michael@michaelmerrill.io>
2017-02-24 12:37:07 -0800

    Implement cors

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,8 +1,14 @@
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const jwt = require('express-jwt')
+const cors = require('cors')
+
+const corsOptions = {
+  origin: 'https://api.snapflixapp.com'
+}
 
 module.exports = (app) => {
+  app.use(cors(corsOptions)) // https://github.com/expressjs/cors
   app.use(bodyParser.urlencoded({ extended: true })) // https://github.com/expressjs/body-parser
   app.use(bodyParser.json()) // https://github.com/expressjs/body-parser
   app.use(morgan('dev')) // https://github.com/expressjs/morgan

82dd5927408ce3c1c0aa407d86107f01a7ea9e41
michaelmerrill <michael@michaelmerrill.io>
2017-02-24 12:56:10 -0800

    Whitelist client for cors headers

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -3,10 +3,13 @@ const morgan = require('morgan')
 const jwt = require('express-jwt')
 const cors = require('cors')
 
+const whitelist = ['https://snapflixapp.com', 'https://staging.snapflixapp.com', 'http://localhost:8080']
 const corsOptions = {
-  origin: 'https://api.snapflixapp.com'
+  origin: function (origin, callback) {
+    const originIsWhitelisted = whitelist.indexOf(origin) !== -1
+    callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted)
+  }
 }
-
 module.exports = (app) => {
   app.use(cors(corsOptions)) // https://github.com/expressjs/cors
   app.use(bodyParser.urlencoded({ extended: true })) // https://github.com/expressjs/body-parser

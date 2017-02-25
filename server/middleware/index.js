bc5c0e8e9d475974df5ee6265630f01700158c42
michaelmerrill <michael@michaelmerrill.io>
2017-02-25 11:16:22 -0800

    Enable cors w/ pre-flight

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -2,26 +2,18 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const jwt = require('express-jwt')
 const cors = require('cors')
-const { writeResponse } = require('../utils')
-
-const whitelist = ['snapflixapp.com', 'staging.snapflixapp.com', 'localhost:3000', 'localhost:8080']
-const corsOptions = {
-  origin: (origin, callback) => {
-    const originIsWhitelisted = whitelist.indexOf(origin) !== -1
-    callback(originIsWhitelisted ? null : origin, originIsWhitelisted)
-  }
-}
 
 module.exports = (app) => {
-  // really dumb work around to fix this issue: https://github.com/expressjs/cors/issues/71
-  app.use((req, res, next) => {
-    writeResponse(res, { origin: req.headers.origin, host: req.headers.host }, 200)
-    // req.headers.origin = req.headers.origin || req.headers.host
-    // next()
-  })
-  app.use(cors(corsOptions)) // https://github.com/expressjs/cors
-  app.use(bodyParser.urlencoded({ extended: true })) // https://github.com/expressjs/body-parser
-  app.use(bodyParser.json()) // https://github.com/expressjs/body-parser
-  app.use(morgan('dev')) // https://github.com/expressjs/morgan
+  // https://github.com/expressjs/cors
+  app.options('*', cors())
+
+  // https://github.com/expressjs/body-parser
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+
+  // https://github.com/expressjs/morgan
+  app.use(morgan('dev'))
+
+  // https://github.com/auth0/express-jwt
   app.use(jwt({ secret: process.env.JWT_SECRET }).unless({ path: ['/', '/auth/login', '/auth/register'] }))
 }
